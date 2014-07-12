2cc5dcf263bf1e4f43566c588c961fe82977d26f
David Leonard <sephirothcloud1025@yahoo.com>
2014-07-12 15:41:39 -0400

    Intermediate work on fetching all issues given a list of repositories

--- get_issues.py
+++ get_issues.py
@@ -1,15 +1,33 @@
 # Module for getting issues from a repository. 
 
 import requests
-import simplejson as json
+import json
+import gitparsers
 
 # Endpoint for getting issues from a repository: 
 #           GET /repos/:owner/:repo/issues
 
 # Pull data from one of my repositories
-req_url = "https://api.github.com/repos/DrkSephy/Deep-Learning/issues"
+#req_url = "https://api.github.com/repos/DrkSephy/Deep-Learning/issues"
 # Make request
-req = requests.get(req_url)
+#req = requests.get(req_url)
 # Store issues
-issues = json.loads(req.content)
-print issues
+#issues = json.loads(req.content)
+#print issues
+
+
+url = "https://api.github.com/users/DrkSephy/repos"
+request = requests.get(url)
+repos = json.loads(request.content)
+repositories = gitparsers.parse_repositories(repos)
+
+new_list = []
+for repository in repositories:
+    req_url = repository + "/issues"
+    print req_url
+    req = requests.get(req_url)
+    if req.status_code == 200:
+
+
+
+
