d0ca8120ff0a7f45b4fb3f7e65718341c2f6312e
David Leonard <sephirothcloud1025@yahoo.com>
2014-07-12 17:41:39 -0400

    Adding method for getting all issues from a user's public repositories

--- get_issues.py
+++ get_issues.py
@@ -21,6 +21,28 @@ def get_issues(url):
     issues = json.loads(req.content)
     return issues
 
+def get_all_issues(repositories):
+    """
+    Grabs a list of all issues from all public repositories of a User.
+
+    Parameters:
+    -----------
+        repositories: A list containing all urls to grab issues from
+
+    Returns:
+    --------
+        issues: A list containing all issues from a list of repositories of a user.
+    """
+    
+    new_list =[]
+    for repository in repositories:
+        req_url = "https://api.github.com/repos/" + repository + "/issues" 
+        req = requests.get(req_url)
+        if req.status_code == 200:
+            issues = json.loads(req.content)
+            new_list.append(issues)
+    return new_list
+
 
 
 
