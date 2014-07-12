b0a1f293e612cee3af49c23dcaa57b2704edbd10
David Leonard <sephirothcloud1025@yahoo.com>
2014-07-12 17:41:02 -0400

    Adding method for getting a list of issues from a single repository

--- get_issues.py
+++ get_issues.py
@@ -4,32 +4,22 @@ import requests
 import json
 import gitparsers
 
-# Endpoint for getting issues from a repository: 
-#           GET /repos/:owner/:repo/issues
+def get_issues(url):
+    """
+    Grabs all issues from a single repository.
 
-# Pull data from one of my repositories
-#req_url = "https://api.github.com/repos/DrkSephy/Deep-Learning/issues"
-# Make request
-#req = requests.get(req_url)
-# Store issues
-#issues = json.loads(req.content)
-#print issues
+    Parameters:
+    -----------
+        url: Github endpoint to pull issues from.
 
-
-url = "https://api.github.com/users/DrkSephy/repos
-request = requests.get(url)
-repos = json.loads(request.content)
-repositories = gitparsers.parse_repositories(repos)
-
-new_list = []
-for repository in repositories:
-    req_url = "https://api.github.com/repos/" + repository + "/issues" 
+    Returns:
+    -------
+        issues: List containing all issues from a repository.
+    """
+    req_url = url
     req = requests.get(req_url)
-    if req.status_code == 200:
-        issues = json.loads(req.content)
-        new_list.append(issues)
-
-print new_list
+    issues = json.loads(req.content)
+    return issues
 
 
 
