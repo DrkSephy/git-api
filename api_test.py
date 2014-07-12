3fdb5d065a1e83029600e8215fa968f14755e629
David Leonard <sephirothcloud1025@yahoo.com>
2014-07-11 22:27:59 -0400

    Parsing a list of my public repositories

--- api_test.py
+++ api_test.py
@@ -1,3 +1,4 @@
+import re
 import requests
 import simplejson as json
 
@@ -10,4 +11,23 @@ req = requests.get(req_url)
 # Return JSON data
 repos = json.loads(req.content)
 # Display data
-print repos
+# print repos
+
+# The keys we are interested in parsing out are:
+#   'full_name': 'DrkSephy/yabe'
+
+keys = ['full_name']
+repositories = []
+
+for a in repos:
+    new_list = {}
+    for k,v in a.iteritems():
+        if k in keys:
+            if re.match('(.*?)(?=\s<)', v) == None:
+                new_list[k] = v
+            else:
+                v2 = re.match('(.*?)(?=\s<)', v)
+                new_list[k] = v2.group() 
+    repositories.append(new_list)
+
+print repositories
