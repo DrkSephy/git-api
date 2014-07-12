df922097d0169c4a68534ea7b3edc92131083126
David Leonard <sephirothcloud1025@yahoo.com>
2014-07-12 15:02:02 -0400

    Refactoring repository parser method into gitparsers.py

---
+++ gitparsers.py
@@ -0,0 +1,31 @@
+# Module containing parsers for data returned by Github's API.
+
+import re
+
+def parse_repositories(data):
+    """
+    Parses returned JSON for the API call to the `repos` endpoint. 
+
+    Parameters:
+    ----------
+    data: JSON data returned from the endpoint. 
+
+    Returns:
+    --------
+    repositories: list
+        - A JSON formatted list containing repository endpoints.
+    """
+
+    keys = ['full_name']
+    repositories = []
+
+    for a in data:
+        new_list = {}
+        for k,v in a.iteritems():
+            if k in keys:
+                if re.match('(.*?)(?=\s<)', v) == None:
+                    repositories.append("https://github.com/" + v)
+                else:
+                    v2 = re.match('(.*?)(?=\s<)', v)
+                    repositories.append("https://github.com/" + v2.group())
+    return repositories
