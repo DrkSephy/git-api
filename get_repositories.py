4394990f3e498ddb445203c227b0a4998c725471
David Leonard <sephirothcloud1025@yahoo.com>
2014-07-12 15:02:51 -0400

    Refactoring get_repositories

--- get_repositories.py
+++ get_repositories.py
@@ -1,8 +1,8 @@
 # Module for getting a list of public repositories belonging to a User.
 
-import re
 import requests
 import simplejson as json
+import gitparsers
 
 # Github requests begin with: ""https://api.github.com/"
 
@@ -16,33 +16,5 @@ repos = json.loads(req.content)
 # Sample Key:Value pair that we are interested in:
 #   'full_name': 'DrkSephy/yabe'
 
-def parse_repositories(data):
-    """
-    Parses returned JSON for the API call to the `repos` endpoint. 
-
-    Parameters:
-    ----------
-    data: JSON data returned from the endpoint. 
-
-    Returns:
-    --------
-    repositories: list
-        - A JSON formatted list containing repository endpoints.
-    """
-
-    keys = ['full_name']
-    repositories = []
-
-    for a in data:
-        new_list = {}
-        for k,v in a.iteritems():
-            if k in keys:
-                if re.match('(.*?)(?=\s<)', v) == None:
-                    repositories.append("https://github.com/" + v)
-                else:
-                    v2 = re.match('(.*?)(?=\s<)', v)
-                    repositories.append("https://github.com/" + v2.group())
-    return repositories
-
-data = parse_repositories(repos)
+data = gitparsers.parse_repositories(repos)
 print data
