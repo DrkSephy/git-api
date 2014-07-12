0c624fb5af43f7363f1072a0d949b0787dc2ae15
David Leonard <sephirothcloud1025@yahoo.com>
2014-07-12 15:20:14 -0400

    Adding notes on getting issues

--- get_issues.py
+++ get_issues.py
@@ -3,8 +3,13 @@
 import requests
 import simplejson as json
 
+# Endpoint for getting issues from a repository: 
+#           GET /repos/:owner/:repo/issues
 
+# Pull data from one of my repositories
 req_url = "https://api.github.com/repos/DrkSephy/Deep-Learning/issues"
+# Make request
 req = requests.get(req_url)
+# Store issues
 issues = json.loads(req.content)
 print issues
